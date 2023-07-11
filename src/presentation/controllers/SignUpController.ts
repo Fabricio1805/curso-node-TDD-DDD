f750608df40475776d90a39cb5c2094dc985bea6
fabricio <fabricio.ss09@outlook.com>
2023-07-11 12:41:07 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,9 +1,8 @@
-import { MissingParamError } from '../errors/missing-param-error';
+import { MissingParamError, InvalidParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helper';
 import { IController } from '../protocols/controller';
 import { IEmailValidator } from '../protocols/email-validator';
 import { IHttpRequest, IHttpResponse} from '../protocols/http';
-import { InvalidParamError } from '../errors/invalid-param-error';
 export class SignUpController implements IController {
 
   constructor(private readonly emailValidator: IEmailValidator) {
@@ -30,7 +29,7 @@ export class SignUpController implements IController {
         return badRequest(new InvalidParamError('email'));
       }
     } catch (error) {
-      console.error(error);
+      //console.error(error);
       return serverError();
     }
   }
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './SignUpController';
-import { MissingParamError } from '../errors/missing-param-error';
-import { InvalidParamError } from '../errors/invalid-param-error';
 import { IEmailValidator } from '../protocols/email-validator';
-import { ServerError } from '../errors/server-error';
+import { MissingParamError, InvalidParamError, ServerError } from '../errors';
 
 interface ISutTypes {
   sut: SignUpController;
