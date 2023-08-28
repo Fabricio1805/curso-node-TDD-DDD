ec76e82af25c2991b138eba9fb7415befb56e7d1
fabricio <fabricio.ss09@outlook.com>
2023-08-28 17:49:12 -0300

    refactor: update mongodb

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { Collection, MongoClient } from 'mongodb';
+import {  Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -16,11 +16,7 @@ export const MongoHelper = {
     this.client = null;
   },
 
-  async getCollection(name: string): Promise<Collection> {
-    if (!this.client) {
-      await this.connect(this.uri);
-    }
-    
+  getCollection(name: string): Collection {   
     return this.client.db().collection(name);
   }
 };
--- src/presentation/protocols/http.ts
+++ src/presentation/protocols/http.ts
@@ -1,8 +1,10 @@
 export interface IHttpResponse {
   statusCode: number
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body: any
 }
 
 export interface IHttpRequest {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   body?: any 
 }
