216057a2017dda9ed3d0ca113f12fe6d49a26775
Fabricio1805 <fabricio.ss09@outlook.com>
2023-08-28 21:30:57 -0300

    feat: ensure LoginContrller returns 400 if no emails is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -3,11 +3,13 @@ import { badRequest } from '../../helpers/http-helper';
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols';
 
 export class LoginController implements IController {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    return new Promise((resolve) =>
-      resolve(badRequest(new MissingParamError('email')))
-    );
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'));
+    }
+
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'));
+    }
   }
-  
 }
