9fdbee0e32cc6cf555e8cb912ab5a80cbce037d9
fabricio <fabricio.ss09@outlook.com>
2023-09-26 09:52:36 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,10 +1,11 @@
+import { IAuthentication } from '../../../domain/usecases/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest, serverError } from '../../helpers/http-helper';
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols';
 import { IEmailValidator } from '../signup/signup-protocols';
 
 export class LoginController implements IController {
-  constructor(private readonly emailValidator: IEmailValidator) { }
+  constructor(private readonly emailValidator: IEmailValidator, private readonly authentication: IAuthentication ) { }
   
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
@@ -21,6 +22,8 @@ export class LoginController implements IController {
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      await this.authentication.auth(email, password);
     } catch (error) {
       return serverError(error);
     }
